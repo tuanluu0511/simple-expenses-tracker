a07a8937415414a50d92ba7cc906390b8c605d33
Tuan Luu <tuanluu.hcmc@gmail.com>
2022-03-15 16:16:01 +0700

    Controlled vs Uncontrolled components and staless and stateful components

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,9 +1,12 @@
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
+import ExpensesFilter from './ExpensesFilter';
+import React, { useState } from 'react';
 
 function Expenses(props) {
   const expenses = props.expenses;
+  const [filteredYear, setFilteredYear] = useState('2020');
 
   function generateExpense(expenses) {
     return (
@@ -16,32 +19,22 @@ function Expenses(props) {
     );
   }
 
+  const filterChangeHandler = (selectedYear) => {
+    setFilteredYear(selectedYear);
+  };
+
   return (
-    <Card className="expenses">
-      {expenses.map((exp) => {
-        return generateExpense(exp);
-      })}
-      {/* <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      ></ExpenseItem> */}
-    </Card>
+    <div>
+      <Card className="expenses">
+        <ExpensesFilter
+          selected={filteredYear}
+          onChangeFilter={filterChangeHandler}
+        />
+        {expenses.map((exp) => {
+          return generateExpense(exp);
+        })}
+      </Card>
+    </div>
   );
 }
 
